c9ea5be14825bffc2a68e6335e332c0b8dad85b9
bomboklada <rada.lahoda08@gmail.com>
2025-01-10 13:06:08 +0100

    init

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
 // todo - Catch my Number 2 (aka I'll guess your number)
 // https://teams.microsoft.com/l/message/19:71800b87e64245d49c76a007e0da22d3@thread.tacv2/1734088800848?tenantId=e7bf04a2-63c8-4a47-a0bd-28d0aa88f6ec&groupId=434779cc-d799-4d77-acb3-b5db8df2dd7f&parentMessageId=1734088800848&teamName=P1A%20ZAL%20(KZD)&channelName=2024%20ZAL%20(KZD)&createdTime=1734088800848
 
-basic.forever(function() {
-    
-})
+basic.showIcon(IconNames.Heart)
